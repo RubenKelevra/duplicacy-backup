c8805d091e49ecbc0cedfd7ce8bdf7527b968687
@RubenKelevra <cyrond@gmail.com>
2022-04-25 14:10:11 +0200

    convert restic specifics to duplicacy specifics; delete tmp files ealier; add a local excludes file

--- makebackup.sh
+++ makebackup.sh
@@ -20,7 +20,8 @@ set -e
 USER=user
 HOSTNAME=hostname
 SERVER=server
-GLOBAL_EXCLUDE="~/makebackup.excludes"
+GLOBAL_EXCLUDE="$HOME/makebackup_global.excludes"
+LOCAL_EXCLUDE="$HOME/makebackup_local.excludes"
 
 KEEP_WITHIN='1:2' # all snapshots for 2 days; daily afterwards
 KEEP_DAILY='7:62' # purge to weekly after two month
@@ -28,29 +29,35 @@ KEEP_WEEKLY='30:1424' # pruge to monthly after 4 years
 KEEP_MONTHLY="356:2880" # purge to yearly after 4 years
 KEEP_YEARLY="0:35244" # 99 years
 
-# make sure this directory exist and you have write access
-CACHEDIR='/var/cache/restic/'
+# directory of the duplicacy cache to add it to the filter
+CACHEDIR_USER='-/home/*/.duplicacy/cache'
+CACHEDIR_ROOT='-/root/.duplicacy/cache'
 
-# set capability for reading all files (this avoid that restic needs to be run as root)
-sudo setcap cap_dac_read_search=+ep /usr/bin/restic
+# set capability for reading all files (this avoid that duplicacy needs to be run as root)
+sudo setcap cap_dac_read_search=+ep /usr/bin/duplicacy
 
 echo "Generating exclude lists..."
 
 # fetch all files currently supplied by packages
-rm -f /tmp/restic-backup.pkg_files
+rm -f /tmp/duplicacy-backup.pkg_files
 while IFS= read -r -d $'\n' filepath; do
-	[ -f "$filepath" ] && echo "$filepath" >> /tmp/restic-backup.pkg_files
+	[ -f "$filepath" ] && echo "$filepath" >> /tmp/duplicacy-backup.pkg_files
 done < <(sudo pacman -Ql | cut -f 1 -d ' ' --complement)
 
 # check all files supplied by packages for changes, and write the changed files to a list
-sudo paccheck --md5sum --quiet --db-files --noupgrade --backup | awk '{ print $2 }' | sed "s/'//g" > /tmp/restic-backup.changed_files
+sudo paccheck --md5sum --quiet --db-files --noupgrade --backup | awk '{ print $2 }' | sed "s/'//g" > /tmp/duplicacy-backup.changed_files
 
 # backup the changed files (remove them from the blacklist)
-grep -v -x -f /tmp/restic-backup.changed_files /tmp/restic-backup.pkg_files | sed 's/\[/\\[/g' > /tmp/restic-backup.blacklist
+grep -v -x -f /tmp/duplicacy-backup.changed_files /tmp/duplicacy-backup.pkg_files | sed 's/\[/\\[/g' > /tmp/duplicacy-backup.blacklist
+
+rm -f /tmp/duplicacy-backup.pkg_files 2>/dev/null || true
+rm -f /tmp/duplicacy-backup.changed_files 2>/dev/null || true
 
 # add the global exclude list to the black list
-cat "$GLOBAL_EXCLUDE" >> /tmp/restic-backup.blacklist
-echo "$CACHEDIR" >> /tmp/restic-backup.blacklist
+cat "$GLOBAL_EXCLUDE" >> /tmp/duplicacy-backup.blacklist
+cat "$LOCAL_EXCLUDE" >> /tmp/duplicacy-backup.blacklist
+echo "$CACHEDIR_USER" >> /tmp/duplicacy-backup.blacklist
+echo "$CACHEDIR_ROOT" >> /tmp/duplicacy-backup.blacklist
 
 echo "Generating package lists..."
 
@@ -58,16 +65,17 @@ echo "Generating package lists..."
 sudo pacman -Qne | sudo tee /.explicit_packages.list >/dev/null
 sudo pacman -Qme | sudo tee /.explicit_foreign_packages.list >/dev/null
 
+echo "Move blacklist to duplicacies 'filters' file location..."
+[ ! -z "$HOME" ] && cat /tmp/duplicacy-backup.blacklist > "$HOME/.duplicacy/filters" || echo "Error, HOME variable was empty"; exit 1
+
+rm -f /tmp/duplicacy-backup.blacklist 2>/dev/null || true
+
 echo "Backing up..."
-restic -r sftp:$server:/home/$user/backups/$hostname --verbose --cache-dir="$cachdir" backup / --exclude-file=/tmp/restic-backup.blacklist
+duplicacy backup -id "$hostname" 
 
 echo "Full system-backup done. Forgetting old snapshots..."
 duplicacy prune -storage "$storage_id" -id "$hostname" -collect-only -keep "$KEEP_WITHIN" -keep "$KEEP_DAILY" -keep "$KEEP_WEEKLY" -keep "$KEEP_MONTHLY" -keep "$KEEP_YEARLY"
 
-sudo setcap cap_dac_read_search=-ep /usr/bin/restic
-
-rm -f /tmp/restic-backup.pkg_files 2>/dev/null || true
-rm -f /tmp/restic-backup.changed_files 2>/dev/null || true
-rm -f /tmp/restic-backup.blacklist 2>/dev/null || true
+sudo setcap cap_dac_read_search=-ep /usr/bin/duplicacy
 
 echo "Operation completed."
