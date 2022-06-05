adea0fd06d177318cda6a798e7017bb3aeaa4881
@RubenKelevra <cyrond@gmail.com>
2022-06-06 00:45:08 +0200

    add elapse output; add more output about the progress

--- makebackup.sh
+++ makebackup.sh
@@ -54,6 +54,7 @@ echo -ne "=> cleanup..."
 rm -f /tmp/duplicacy-backup.pkg_files 2>/dev/null || true
 echo " done."
 
+start_time="$(date +%s)"
 first=true
 while IFS= read -r -d $'\n' filepath; do
 	if $first; then
@@ -61,44 +62,80 @@ while IFS= read -r -d $'\n' filepath; do
 		first=false
 	fi
 	[ -f "$filepath" ] && echo "$filepath" >> /tmp/duplicacy-backup.pkg_files
-done < <(sudo pacman -Ql | cut -f 1 -d ' ' --complement)
-echo " done."
+done < <(pacman -Ql | cut -f 1 -d ' ' --complement)
+end_time="$(date +%s)"
+echo " done after $((start_time-end_time)) seconds"
+unset start_time end_time
 
 # check all files supplied by packages for changes, and write the changed files to a list
+start_time="$(date +%s)"
+echo -ne "=> check files managed by pacman for changes..."
 paccheck --md5sum --quiet --db-files --noupgrade --backup | awk '{ print $2 }' | sed "s/'//g" > /tmp/duplicacy-backup.changed_files
+end_time="$(date +%s)"
+echo " done after $((start_time-end_time)) seconds"
+unset start_time end_time
 
 
 # backup the changed files (remove them from the blacklist)
+start_time="$(date +%s)"
+echo -ne "=> generating pacman supplied files blacklist..."
 grep -v -x -f /tmp/duplicacy-backup.changed_files /tmp/duplicacy-backup.pkg_files | sed 's/\[/\\[/g' | sed 's/^\//-/g' > /tmp/duplicacy-backup.blacklist
+end_time="$(date +%s)"
+echo " done after $((start_time-end_time)) seconds"
+unset start_time end_time
 
 rm -f /tmp/duplicacy-backup.pkg_files 2>/dev/null || true
 rm -f /tmp/duplicacy-backup.changed_files 2>/dev/null || true
 
 # add the global exclude list to the black list
+echo -ne "=> finishing blacklist generation..."
 { cat "$GLOBAL_EXCLUDE"; cat "$LOCAL_EXCLUDE"; echo "$CACHEDIR_USER"; echo "$CACHEDIR_ROOT"; } >> /tmp/duplicacy-backup.blacklist
-
-echo "Generating package lists..."
+echo " done."
 
 # generate package-lists for native and foreign packages, to be able to restore the system from a mirror
-sudo pacman -Qne | sudo tee /.explicit_packages.list >/dev/null
-sudo pacman -Qme | sudo tee /.explicit_foreign_packages.list >/dev/null
+start_time="$(date +%s)"
+echo -ne "=> generating list of installed packages and their versions..."
+pacman -Qne | sudo tee /.explicit_packages.list >/dev/null
+pacman -Qme | sudo tee /.explicit_foreign_packages.list >/dev/null
+end_time="$(date +%s)"
+echo " done after $((start_time-end_time)) seconds"
+unset start_time end_time
 
-echo "Move blacklist to duplicacies 'filters' file location..."
+echo -ne "=> move blacklist to duplicacies 'filters' file location..."
 if [ -n "$HOME" ]; then
 	cat /tmp/duplicacy-backup.blacklist > "$HOME/.duplicacy/filters"
 else
 	echo "Error, HOME variable was empty"
 	exit 1
 fi
+echo " done."
 
+echo -ne "=> cleanup..."
 rm -f /tmp/duplicacy-backup.blacklist 2>/dev/null || true
+echo " done."
 
-echo "Backing up..."
+start_time="$(date +%s)"
+echo "=> running duplicacy:
+"
 duplicacy backup -storage "$BACKUP_STORAGE" -threads 4
+end_time="$(date +%s)"
+echo "
+duplicacy completed it's run after $((start_time-end_time)) seconds"
+unset start_time end_time
 
-echo "Full system-backup done. Forgetting old snapshots..."
+start_time="$(date +%s)"
+echo -ne "=> checking storage..."
 duplicacy check -storage "$BACKUP_STORAGE" -id "$HOSTNAME" -fossils -resurrect -threads 4
+end_time="$(date +%s)"
+echo " done after $((start_time-end_time)) seconds"
+unset start_time end_time
+
+start_time="$(date +%s)"
+echo -ne "=> pruning storage..."
 duplicacy prune -storage "$BACKUP_STORAGE" -id "$HOSTNAME" -keep "$KEEP_WITHIN" -keep "$KEEP_DAILY" -keep "$KEEP_WEEKLY" -keep "$KEEP_MONTHLY" -keep "$KEEP_YEARLY" -threads 4
+end_time="$(date +%s)"
+echo " done after $((start_time-end_time)) seconds"
+unset start_time end_time
 
 sudo setcap cap_dac_read_search=-ep /usr/bin/duplicacy
 sudo setcap cap_dac_read_search=-ep /usr/bin/paccheck
